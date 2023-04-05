0a7addf0fda6bf0806e9134677fe0add1b8cf7d6
Tina Hammar <tinahammar@gmail.com>
2023-04-05 12:04:55 +0200

    chatgpt4 updated the code :+1:

--- resources/dist/js/component.js
+++ resources/dist/js/component.js
@@ -4,159 +4,137 @@ document.addEventListener('alpine:init', () => {
         filename: '',
         filetype: '',
         width: config.width,
-        height:config.height,
+        height: config.height,
         shape: config.shape,
         statePath: config.statePath,
         aspectRatio: config.aspectRatio,
         appliedAspectRatio: config.aspectRatio,
         rotatable: config.rotatable,
         rotateDegree: config.rotateDegree,
-        dragMode : config.dragMode,
+        dragMode: config.dragMode,
         viewMode: config.viewMode,
         zoomable: config.zoomable,
 
-        scales:{
-
+        scales: {
             flipHorizontal: 1,
             flipVertical: 1,
         },
 
         cropper: null,
-        init() {
-            this.$nextTick(
-                () => {
 
-                }
-            )
-            this.$watch('files', async (value) => {
-
-                let ref = this;
-
-                let reader, files = this.files
-                if( files == null ||files[0] == undefined){
+        async init() {
+            this.$watch('files', async (files) => {
+                if (files === null || files[0] === undefined) {
                     return;
                 }
-                this.filename = files[0].name;
-                this.filetype = files[0].type
 
-                reader = new FileReader()
+                const reader = new FileReader();
                 reader.onload = (e) => {
-                    ref.$refs.cropper.src = e.target.result;
+                    this.$refs.cropper.src = e.target.result;
+
                     setTimeout(() => {
-                        if(this.cropper != null){
+                        this.destroyCropper();
+                        this.initCropper();
+                    }, 500);
+                };
 
-                        }
-                        ref.destroyCropper();
-                        ref.initCropper()
-                    }, 500)
-                }
-                await reader.readAsDataURL(files[0])
-
-
-            })
+                this.filename = files[0].name;
+                this.filetype = files[0].type;
+                await reader.readAsDataURL(files[0]);
+            });
         },
 
-        destroyCropper(){
-            if(this.cropper == null)
+        destroyCropper() {
+            if (this.cropper === null) {
                 return;
+            }
+
             this.cropper.destroy();
             this.cropper = null;
         },
 
         async initCropper() {
-            this.cropper = new Cropper(
-                this.$refs.cropper, {
-                    aspectRatio: this.aspectRatio,
-                    rotatable: this.rotatable,
-                    dragMode: this.dragMode,
-                    viewMode: this.viewMode,
-                    zoomable: this.zoomable,
-                    crop(event) {
-
-                    },
-                })
+            this.cropper = new Cropper(this.$refs.cropper, {
+                aspectRatio: this.aspectRatio,
+                rotatable: this.rotatable,
+                dragMode: this.dragMode,
+                viewMode: this.viewMode,
+                zoomable: this.zoomable,
+                crop(event) {},
+            });
         },
 
-        rotateByValue(value){
+        rotateByValue(value) {
             const previousRotate = this.cropper.getImageData().rotate;
-            this.cropper.rotate(value-previousRotate)
-        },
-        resetRotate(){
-            let previousRotate = this.cropper.getImageData().rotate;
-            previousRotate = (previousRotate )-(previousRotate * 2);
-            this.rotateDegree = 0;
-            this.cropper.rotate(previousRotate)
+            this.cropper.rotate(value - previousRotate);
         },
 
-        setAspectRatio(ratio){
+        resetRotate() {
+            let previousRotate = this.cropper.getImageData().rotate;
+            previousRotate = (previousRotate) - (previousRotate * 2);
+            this.rotateDegree = 0;
+            this.cropper.rotate(previousRotate);
+        },
+
+        setAspectRatio(ratio) {
             this.cropper.setAspectRatio(ratio);
             this.appliedAspectRatio = ratio;
         },
 
-        flip (){
-            if(this.scales.flipVertical < 0){
-                this.scales.flipVertical = 1;
-            }else{
-                this.scales.flipVertical = -1;
-            }
+        flip() {
+            this.scales.flipVertical *= -1;
+            this.scales.flipHorizontal *= -1;
 
-            if(this.scales.flipHorizontal < 0){
-                this.scales.flipHorizontal = 1;
-            }else{
-                this.scales.flipHorizontal = -1;
-            }
-
-            this.cropper.scale(this.scales.flipHorizontal, this.scales.flipVertical)
-        },
-        flipHorizontal (){
-            if(this.scales.flipHorizontal < 0){
-                this.scales.flipHorizontal = 1;
-            }else{
-                this.scales.flipHorizontal = -1;
-            }
-
-            this.cropper.scale(this.scales.flipHorizontal, this.scales.flipVertical)
-        },
-        flipVertical (){
-            if(this.scales.flipVertical < 0){
-                this.scales.flipVertical = 1;
-            }else{
-                this.scales.flipVertical = -1;
-            }
-            this.cropper.scale(this.scales.flipHorizontal, this.scales.flipVertical)
+            this.cropper.scale(this.scales.flipHorizontal, this.scales.flipVertical);
         },
 
-        zoomByValue(value){
-            this.cropper.zoom(value)
+        flipHorizontal() {
+            this.scales.flipHorizontal *= -1;
+            this.cropper.scale(this.scales.flipHorizontal, this.scales.flipVertical);
+        },
+
+        flipVertical() {
+            this.scales.flipVertical *= -1;
+            this.cropper.scale(this.scales.flipHorizontal, this.scales.flipVertical);
+        },
+
+        zoomByValue(value) {
+            this.cropper.zoom(value);
         },
 
         uploadCropperImage(){
-            let ref = this;
+
+            const input = document.getElementById(this.statePath).querySelector('input[type="file"]');
+            const fileName = this.filename;
+            const fileType = this.filetype;
 
             this.cropper.getCroppedCanvas({
-                maxWidth: ref.width,
-                maxHeight: ref.height,
+                maxWidth: this.width,
+                maxHeight: this.height,
             }).toBlob((croppedImage) => {
-
-                let input = document.getElementById(ref.statePath).getElementsByTagName('input')[0]
                 let event = new Event('change');
-                let fileName = ref.filename;
-                let filetype = ref.filetype;
                 let file = new File(
                     [croppedImage],
                     fileName,
-                    {type:filetype, lastModified:new Date().getTime()},
+                    {type:fileType, lastModified:new Date().getTime()},
                     'utf-8'
                 );
 
                 let container = new DataTransfer();
                 container.items.add(file);
-
                 input.files = container.files;
-                ref.$dispatch("close-modal", {id: "cropper-modal-"+ref.statePath, files: null})
-                input.dispatchEvent(event);
-            },  ref.filetype);
 
-        }
+                // close the modal
+                this.$dispatch('close-modal', { id: `cropper-modal-${this.statePath}`, files: null })
+                input.dispatchEvent(event);
+
+                // reset the component state
+                this.showCropper = false;
+                this.filename = '';
+                this.filetype = '';
+                this.cropper.destroy();
+                this.cropper = null;
+            },  fileType);
+        },
     }))
 })
