76c840a2e4d9dc7d46956a7b4580189dd9f075a8
Tina Hammar <tinahammar@gmail.com>
2023-04-12 16:04:25 +0200

    filament v3

--- src/FilamentCropperServiceProvider.php
+++ src/FilamentCropperServiceProvider.php
@@ -2,29 +2,39 @@
 
 namespace Nuhel\FilamentCropper;
 
-use Filament\PluginServiceProvider;
+
+use Filament\Support\Assets\AlpineComponent;
+use Filament\Support\Assets\AssetManager;
+use Filament\Support\Assets\Css;
+use Filament\Support\Assets\Js;
 use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
 
-class FilamentCropperServiceProvider extends PluginServiceProvider
+class FilamentCropperServiceProvider extends PackageServiceProvider
 {
-    public static string $name = 'filament-cropper';
-
-    protected array $styles = [
-        'filament-cropper-style' => __DIR__ . '/../resources/dist/css/filament-cropper.css',
-    ];
-
-    protected array $scripts = [
-        'filament-cropper-script' => __DIR__ . '/../resources/dist/js/filament-cropper.js',
-    ];
-
-    protected array $beforeCoreScripts = [
-        'filament-croppie-cropper-component-script' => __DIR__ . '/../resources/dist/js/component.js',
-    ];
 
     public function configurePackage(Package $package): void
     {
-        $package->name(static::$name)
+        $package
+            ->name('filament-cropper')
             ->hasAssets()
             ->hasViews();
     }
+
+    public function packageRegistered(): void
+    {
+        $this->app->resolving(AssetManager::class, function () {
+
+            \Filament\Support\Facades\FilamentAsset::register([
+                Css::make('cropper-css', 'https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.13/cropper.min.css'),
+                Js::make('cropper-js', 'https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.13/cropper.min.js'),
+
+                Css::make('filament-cropper-css', __DIR__ . '/../resources/dist/css/filament-cropper.css'),
+                AlpineComponent::make('filament-cropper', __DIR__ . '/../resources/dist/js/filament-cropper.js'),
+
+            ], 'nuhel/filament-cropper');
+
+        });
+    }
+
 }
